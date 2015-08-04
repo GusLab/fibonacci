97b9a01c00ca395e5bb82317e4c9aa3025d4f1a2
fitg <gustaw.fit@gmail.com>
2015-08-04 18:40:48 +0100

    added unit test stubs

---
+++ Fibonacci.Tests/Runners.Tests/IterativeFibonacci.Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci.Tests.Runners.Tests
+{
+    [TestClass]
+    public class IterativeFibonacciTest
+    {
+        [TestMethod]
+        public void IterativeShouldReturn15FirstElements()
+        {
+            //var iterativeImpl = new Iter
+        }
+    }
+}
